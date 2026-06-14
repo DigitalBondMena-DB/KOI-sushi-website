1d1268ea2398cf74e1d3e2357e36d6497a914488
ahmedWessamTest <ahmedwessam202@gmail.com>
2026-06-14 11:31:56 +0200

    feat: migrate to Netlify Angular Runtime v4 and implement custom server request handler

--- src/server.ts
+++ src/server.ts
@@ -39,17 +39,29 @@
 // }
 
 // export const reqHandler = createNodeRequestHandler(app);
-import { AngularAppEngine, createRequestHandler } from '@angular/ssr';
-import { getContext } from '@netlify/angular-runtime/context.mjs';
+import { AngularAppEngine, createRequestHandler } from '@angular/ssr'
+import { getAllowedHosts, getContext, getTrustProxyHeaders } from '@netlify/angular-runtime/app-engine.js'
 
-const angularAppEngine = new AngularAppEngine();
+const angularAppEngine = new AngularAppEngine({
+  allowedHosts: getAllowedHosts(),
+  trustProxyHeaders: getTrustProxyHeaders(),
+})
 
 export async function netlifyAppEngineHandler(request: Request): Promise<Response> {
-  const context = getContext();
+  const context = getContext()
 
-  const result = await angularAppEngine.handle(request, context);
+  // Example API endpoints can be defined here.
+  // Uncomment and define endpoints as necessary.
+  // const pathname = new URL(request.url).pathname
+  // if (pathname === '/api/hello') {
+  //   return Response.json({ message: 'Hello from the API' });
+  // }
 
-  return result || new Response('Not found', { status: 404 });
+  const result = await angularAppEngine.handle(request, context)
+  return result || new Response('Not found', { status: 404 })
 }
 
-export const reqHandler = createRequestHandler(netlifyAppEngineHandler);
+/**
+ * The request handler used by the Angular CLI (dev-server and during build).
+ */
+export const reqHandler = createRequestHandler(netlifyAppEngineHandler)
